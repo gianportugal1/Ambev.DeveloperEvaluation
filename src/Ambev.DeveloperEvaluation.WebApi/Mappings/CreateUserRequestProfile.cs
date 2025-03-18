589c50780a37cc84b481cb0a1e61872ecfc5058b
Giancarlos Portugal <gianportugal@yahoo.com.br>
2025-03-18 20:39:39 -0300

    Add project files.

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/CreateUserRequestProfile.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class CreateUserRequestProfile : Profile
+{
+    public CreateUserRequestProfile()
+    {
+        CreateMap<User, CreateUserResult>();
+        CreateMap<CreateUserCommand, User>();
+               
+        CreateMap<User, GetUserResult>();        
+        CreateMap<GetUserResult, GetUserResponse>();        
+    }
+}
